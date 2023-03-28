a1148ef767908ff49a9f29a39e8f7b34fddfab86
lizbeth.tabuena <izzah.tabuena@gmail.com>
2023-03-28 20:53:05 +0800

    'final'

--- src/main/java/com/mycompany/numberguess/NumberGuessView.java
+++ src/main/java/com/mycompany/numberguess/NumberGuessView.java
@@ -4,9 +4,6 @@
  */
 package com.mycompany.numberguess;
 
-import java.awt.EventQueue;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.util.Random;
 
